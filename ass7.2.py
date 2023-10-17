1b68b54625c824eec45a0cc0fbefa04f3f294a4b
LakshmiPrasanna-4 <125474262+LakshmiPrasanna-4@users.noreply.github.com>
2023-10-17 18:14:37 +0530

    Add files via upload

---
+++ ass7.2.py
@@ -0,0 +1,7 @@
+# write a program to print function
+
+n=int(input())
+i=1
+while(i<=n):
+    print(i,end='')
+    i=i+1
---
+++ ass7.3.py
@@ -0,0 +1,7 @@
+# Write a program for arithmetic operators
+
+a=int(input())
+b=int(input())
+print(a+b)
+print(a-b)
+print(a*b)
